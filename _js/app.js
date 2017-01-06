b692b20792058266bcc045b0184518fa06b9453c
doomsterinc <doomsterinc1@gmail.com>
2017-01-06 01:39:46 -0200

    Draw Arc

--- _js/app.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    //Referenciando o Canvas
-    var canvas = document.getElementById('meu_canvas');
-
-    //Obtendo o contexto gráfico
-    var context = canvas.getContext('2d');
-
-};
