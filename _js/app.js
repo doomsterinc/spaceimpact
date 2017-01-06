79cbb2190c9b51b947900114ad8637b06f2ded5a
doomsterinc <doomsterinc1@gmail.com>
2017-01-05 23:32:10 -0200

    First draw in canvas

--- _js/app.js
+++ _js/app.js
@@ -6,12 +6,30 @@ window.onload = function () {
     var context = canvas.getContext('2d');
 
     //Preeenchendo com vermelho
-    context.fillStyle = 'red';
-    context.fillRect(50, 50, 100, 100);
+    //context.fillStyle = 'red';
+    //context.fillRect(50, 50, 100, 100);
 
     //Contorno azul com 3px de espessura
-    context.lineWidth = 3;
-    context.strokeStyle = 'blue';
-    context.strokeRect(50, 50, 100, 100);
+    //context.lineWidth = 3;
+    //context.strokeStyle = 'blue';
+    //context.strokeRect(50, 50, 100, 100);
 
+    //Inicia um novo path (apagando desenhos anteriores)
+    context.beginPath();
+
+    //Desenhando uma estrela
+    //Ponto inicial
+    context.moveTo(75, 250);
+    context.lineTo(150, 50);
+    context.lineTo(225, 250);
+    context.lineTo(50, 120);
+    context.lineTo(250, 120);
+    context.lineTo(75, 250);
+
+    //Configurar a linha;
+    context.lineWidth = 2;
+    context.strokeStyle = 'red';
+
+    //Traçar todas as linhas do caminho
+    context.stroke();
 };
